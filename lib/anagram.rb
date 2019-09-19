8f1077a398108f9f91bd5dad39148589360132b0
Ali Karimi <ajkarimi7@gmail.com>
2019-09-19 17:26:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,6 +2,14 @@
 
 class Anagram 
   
-  
-  
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
 end
